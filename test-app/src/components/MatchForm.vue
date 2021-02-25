d0d1325c8e155e5f067de53cd1bc94b6fb4c75f5
niklastheman <e.niklas.holmstrom@gmail.com>
2021-02-25 22:05:21 +0100

    matches contiued

--- test-app/src/components/MatchForm.vue
+++ test-app/src/components/MatchForm.vue
@@ -15,7 +15,57 @@
             label="Opponent"
             required
             ></v-select>
+            
+            <v-menu
+            ref="menu"
+            v-model="menu"
+            :close-on-content-click="false"
+            :return-value.sync="date"
+            transition="scale-transition"
+            offset-y
+            min-width="auto"
+            >
+                <template v-slot:activator="{ on, attrs }">
+                    <v-text-field
+                        v-model="date"
+                        label="Picker in menu"
+                        prepend-icon="mdi-calendar"
+                        readonly
+                        v-bind="attrs"
+                        v-on="on"
+                    ></v-text-field>
+                </template>
+                <v-date-picker
+                v-model="date"
+                no-title
+                scrollable
+                >
+                    <v-spacer></v-spacer>
+                    <v-btn
+                        text
+                        color="primary"
+                        @click="menu = false"
+                    >
+                        Cancel
+                    </v-btn>
+                    <v-btn
+                        text
+                        color="primary"
+                        @click="$refs.menu.save(date)"
+                    >
+                        OK
+                    </v-btn>
+                </v-date-picker>
+            </v-menu>
 
+           <v-slider
+                v-model="games"
+                :tick-labels="ticksLabels"
+                :max="5"
+                step="1"
+                ticks="always"
+                tick-size="6"
+            ></v-slider>
 
             <v-textarea
             solo
@@ -38,6 +88,18 @@ export default {
     data: () => ({
         valid: true
         , select: null
+        , date: new Date().toISOString().substr(0, 10)
+        , menu: false
+        , value: 0
+        , games: 0
+        , ticksLabels: [
+            1,
+            2,
+            3,
+            4, 
+            5, 
+            6
+        ],
     }),
     computed: {
 
